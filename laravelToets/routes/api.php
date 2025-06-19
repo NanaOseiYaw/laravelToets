6ce060aa754ae30a9e7b87c9277c2f8d697e3ad9
nana.osei <oseinanayaw28@gmail.com>
2025-06-19 09:31:05 +0200

    Update api.php

--- laravelToets/routes/api.php
+++ laravelToets/routes/api.php
@@ -1,9 +1,30 @@
 <?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\TeacherController;
 use App\Http\Controllers\API\SubjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
-Route::apiResource('teachers', TeacherController::class);
-Route::apiResource('subjects', SubjectController::class);
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => '',
+        'password' => ''
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['De inloggegevens kloppen niet.'],
+        ]);
+    }
+
+    return ['token' => $user->createToken('api-token')->plainTextToken];
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('teachers', TeacherController::class);
+    Route::apiResource('subjects', SubjectController::class);
+});
